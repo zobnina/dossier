04978241df91c2d1a9695aaa541d83105ec6d757
Aleksandra Zobnina <aozobnina@vtb.ru>
2023-09-29 14:06:27 +0300

    init

---
+++ src/test/java/ru/neoflex/learning/creaditpipeline/dossier/DossierApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neoflex.learning.creaditpipeline.dossier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DossierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
